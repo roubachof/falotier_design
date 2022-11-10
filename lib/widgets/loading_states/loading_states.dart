35d1a76b46ffd633aa3193b474a6243f4affcf41
roubachof <jm.alfonsi@gmail.com>
2022-11-10 19:15:33 +0000

    add text loading widget

--- lib/widgets/loading_states/loading_states.dart
+++ lib/widgets/loading_states/loading_states.dart
@@ -2,12 +2,28 @@ import 'package:falotier_design/falotier_design.dart';
 import 'package:flutter/material.dart';
 
 class AppLoadingWidget extends StatelessWidget {
-  const AppLoadingWidget({super.key});
+  const AppLoadingWidget({this.loadingMessage, super.key});
+
+  final String? loadingMessage;
 
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return CircularProgressIndicator(color: theme.colors.accent);
+    final loadingIndicator =
+        CircularProgressIndicator(color: theme.colors.accent);
+
+    if (loadingMessage != null) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          loadingIndicator,
+          const AppGap.regular(),
+          AppText.paragraphMedium(loadingMessage!),
+        ],
+      );
+    }
+
+    return loadingIndicator;
   }
 }
 
@@ -50,6 +66,38 @@ class AppErrorWidget extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class AppLoadingText extends StatelessWidget {
+  const AppLoadingText({
+    Key? key,
+    this.loadingMessage = 'loading',
+  }) : super(key: key);
+
+  final String loadingMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<int>(
+      duration: const Duration(seconds: 200),
+      tween: IntTween(begin: 0, end: 200 * 1000),
+      builder: (BuildContext context, value, Widget? child) {
+        // Funny little easy animation
+        String dots = '';
+
+        final loadingDurationMilliseconds = value % 1000;
+        if (loadingDurationMilliseconds > 750) {
+          dots = '...';
+        } else if (loadingDurationMilliseconds > 500) {
+          dots = '..';
+        } else if (loadingDurationMilliseconds > 250) {
+          dots = '.';
+        }
+
+        return AppText.paragraphLarge('$loadingMessage$dots');
+      },
     );
   }
 }
